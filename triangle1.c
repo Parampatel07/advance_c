d0fb17bce51b682baa31ed2a9171aed2708c6dbf
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2023-08-03 17:35:31 +0530

    Create triangle1.c
    assigment

---
+++ triangle1.c
@@ -0,0 +1,33 @@
+// Write a Print following triangle 
+// 1
+// 12
+// 123
+// 1234
+// 12345
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int i = 0;
+    int j = 0;
+    for( j = 1 ; j < 6 ; j++)
+    {
+        
+        for(i = 1; i <= j ; i++)
+        {    
+            cout<<i;
+        }
+        cout<<"\n";  
+    }
+    // cout<<"*";
+    // cout<<"\n";
+    // cout<<"*";
+    // cout<<"*";
+    // cout<<"*";
+    // cout<<"\n";
+    // cout<<"*";
+    // cout<<"*";
+    // cout<<"*";
+    // cout<<"*";
+}
