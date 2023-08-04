ead777fe8a47b06f243f02cfb5df3bab57486d37
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2023-08-04 17:59:16 +0530

    prog 19

---
+++ four_digit.c
@@ -0,0 +1,23 @@
+// Write a C++ program to input any four digits number and display its sum.
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int number = 0;
+    int first = 0;
+    int second = 0 ; 
+    int third = 0 ;
+    int fourth = 0;
+    int answer = 0;
+    cout<<"Enter value of number ";
+    cin>>number;
+    
+    first = number / 1000;
+    second = (number / 100) % 10;
+    third = (number / 10) % 10 ;
+    fourth = number % 10;
+    answer = first + second + third + fourth ;
+  
+    cout<<"The value of answer  is "<<answer;
+}
