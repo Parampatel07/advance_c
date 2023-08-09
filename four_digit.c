af748c298bd03c18b5ccf678409840ae3b7cf1ef
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2023-08-09 17:46:29 +0530

    prog 19

--- four_digit.c
+++ four_digit.c
@@ -4,20 +4,25 @@
 using namespace std;
 int main()
 {
-    int number = 0;
-    int first = 0;
-    int second = 0 ; 
-    int third = 0 ;
-    int fourth = 0;
-    int answer = 0;
+    int number ; 
+    int first , second , third , fourth ;
+    int answer ;
     cout<<"Enter value of number ";
     cin>>number;
     
-    first = number / 1000;
-    second = (number / 100) % 10;
-    third = (number / 10) % 10 ;
-    fourth = number % 10;
-    answer = first + second + third + fourth ;
-    // cout<<"the value of first "<<first;
-    cout<<"The value of answer  is "<<answer;
+    if(number > 999 && number < 10000)
+    {
+        first = number / 1000 ; 
+        second = number / 100 % 10;
+        third = number / 10 % 10;
+        fourth = number % 10;
+        answer = first + second + third + fourth;
+        cout<<"Answer is "<<answer;            
+    }
+    else
+    {
+        cout<<"Enter 4 digit number ";
+    }
+    
+    
 }
